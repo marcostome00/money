cdd7ace6f1dd23e6da5f3312934ad2809707ad4d
Marcos Tomé <marcostome00@gmail.com>
2018-08-18 17:08:04 -0300

    Add metodo create e findOne categoria

--- src/main/java/com/algaworks/algamoneyapi/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.algaworks.algamoneyapi;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
@@ -20,6 +29,25 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}	
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		Categoria categoriaSalva =  categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> findOne(@PathVariable Long codigo) {
+		Optional<Categoria> categoria =  categoriaRepository.findById(codigo);
+		
+		if(categoria.isPresent()) {
+			return ResponseEntity.ok().body(categoria.get());
+		}else {
+			return ResponseEntity.noContent().build();
+		}
+	}
 }
